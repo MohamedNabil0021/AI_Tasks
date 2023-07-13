86c74d935da154cad39d6995ac8877ec0afb4f5d
MohamedNabil0021 <139374687+MohamedNabil0021@users.noreply.github.com>
2023-07-14 02:37:18 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 02:30:46 2023
+
+@author: Mohamed
+"""
+
+# Task1
+
+def summ (n1,n2):
+    print(f"the sum of the two number is {n1+n2}")
+    
+summ(4,8)
+
+
+
