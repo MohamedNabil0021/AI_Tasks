cd47155e4f0be5c0f89312092d04de2aec13cae9
MohamedNabil0021 <139374687+MohamedNabil0021@users.noreply.github.com>
2023-07-20 23:28:28 +0300

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 23:27:25 2023
+
+@author: Mohamed
+"""
+
+#Task2
+
+
+def summ(*n):
+    z=0
+    for i in n:
+        z+=i
+    print(z)
+    
+    
+summ(5,9,5,2,6,4,8,7,9,5)
