1e5ee785ab1b3ee5e48d3720e5171fa8b36aed4f
MohamedNabil0021 <139374687+MohamedNabil0021@users.noreply.github.com>
2023-07-20 23:36:58 +0300

    Add files via upload

---
+++ Task8.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 23:33:34 2023
+
+@author: Mohamed
+"""
+
+#task8
+def squares(*numbers):
+ 	l =[]
+ 	for i in numbers:
+         l.append(i**2)
+ 	print(l)
+		
+squares(1,2,3,5)
+
+
+
+my_string="my name is mohamed"
+My_string="MY NAME IS MOHAMED"
+
+print(my_string.upper())
+print(My_string.lower())
+
+
